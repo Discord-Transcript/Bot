e84e5cc26599a617752ff7a11d092c52230f2584
Mr Tomato <58458857+mrTomatolegit@users.noreply.github.com>
2021-05-13 13:06:28 +0300

    Fix a lot of things
    Made code easier to read
    
    Made code work faster

--- commands/Info/help.js
+++ commands/Info/help.js
@@ -1,4 +1,4 @@
-let Discord = require("discord.js");
+let { MessageEmbed } = require("discord.js");
 let moment = require("moment");
 
 
@@ -13,38 +13,37 @@ module.exports = {
     enabled: true,
     donator: false,
     cooldown: 5000,
-    
 
+    execute(client, message, [commandName], data) {
+        const authorName = message.member && message.member.nickname ? message.member.nickname : message.author.username;
+        if (!commandName) {
+            let embed = new MessageEmbed()
+                .setTitle('Help Menu')
+                .setDescription(`Hello ${authorName}. All my commands is listed below.\nPrefix: t!`)
+                .setFooter(`Requested By ${authorName}`, message.author.displayAvatarURL({ dynamic: true }));
 
+            client.commands.filter(x => x.category !== 'dev').each(x => {
+                embed.addField(x.name, `Category: ${x.category}\n_${x.description}_`)
+            });
 
-    async execute(client, message, args, data) {
-        if(!args[0]){
-       let embed = new Discord.MessageEmbed()
-       .setTitle('Help Menu')
-       .setDescription(`Hello ${!message.member.nickname ? message.author.username : message.member.nickname}. All my commands is listed below.\nPrefix: t!`)
-       .setFooter(`Requested By ${!message.member.nickname ? message.author.tag : message.member.nickname}`, message.author.displayAvatarURL({dynamic:true}))
-       client.commands.filter(x => x.category !== 'dev').each(x => {
-           embed.addField(x.name, `Category: ${x.category}\n_${x.description}_`)
-    });
-    embed.addField("Usefull Links", `[Website](https://dtranscript.cf)`)
-    return message.channel.send(embed);
+            embed.addField("Useful Links", `[Website](https://dtranscript.cf)`);
+
+            return message.channel.send(embed);
         } else {
-            let command = await client.commands.get(args[0]) || await client.commands.find(x => x.aliases && x.aliases.includes(args[0]));
-            let aliases = command.aliases.map(x => x);
-            if(!command.aliases) aliases = 'No Aliases';
-            let embed = new Discord.MessageEmbed()
-            .setTitle(`${command.name} Info`)
-            .addField('Description:', `_${command.description}_`)
-            .addField("Category:", command.category)
-            .addField("Aliases:", aliases.join(",") || 'None')
-            .addField("Usage:", command.usage)
-            .addField("Cooldown", `${moment(command.cooldown).seconds()} Seconds`)
-            .setFooter(`Requested By ${!message.member.nickname ? message.author.tag : message.member.nickname}`, message.author.displayAvatarURL({dynamic:true}))
-       
+            let command = client.commands.get(commandName) || client.commands.find(x => x.aliases && x.aliases.includes(commandName));
+            let aliases = command.aliases || 'No Aliases';
 
-            embed.addField("Usefull Links", `[Website](https://dtranscript.cf)`)
-    return message.channel.send(embed);
-            
+            const embed = new MessageEmbed()
+                .setTitle(`${command.name} Info`)
+                .addField('Description:', `_${command.description}_`)
+                .addField("Category:", command.category)
+                .addField("Aliases:", aliases.join(",") || 'None')
+                .addField("Usage:", command.usage)
+                .addField("Cooldown", `${moment(command.cooldown).seconds()} Seconds`)
+                .addField("Useful Links", `[Website](https://dtranscript.cf)`)
+                .setFooter(`Requested By ${authorName}`, message.author.displayAvatarURL({ dynamic: true }));
+
+            return message.channel.send(embed);
         }
     }
 }
